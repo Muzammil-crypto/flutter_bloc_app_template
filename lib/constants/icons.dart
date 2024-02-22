df6719e1b9c432fb0e164c7f3c700c7973838ea6
Muzammil Rafiq <57823963+Muzammil-crypto@users.noreply.github.com>
2024-02-22 15:03:53 +0500

    testing changes

--- lib/constants/icons.dart
+++ lib/constants/icons.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc_app_template/generated/assets.gen.dart';
 
 class AppIcons {
   AppIcons._();
-
   static const chevronRight = Icons.chevron_right;
   static const iconCheck = Icons.check;
   static const toolbarSettings = Icons.settings;
